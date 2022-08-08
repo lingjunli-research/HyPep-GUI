72c70b257521bf0fce2ef38e0bc112e62c380fea
simonyen0712 <71857621+simonyen0712@users.noreply.github.com>
2022-08-09 00:21:37 +0800

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -31,6 +31,9 @@ with open('SHS_ouput.pkl', 'rb') as file_h:
 
 SHS_results_path = SHS_results_path.rename(columns={"accession": "Accession Number", "Precursor_Sequence": "Sequence"})
 
+with open('discovery_list.pkl', 'rb') as file_discovery:
+    SHS_Discovery_results_path = pickle.load(file_discovery)
+    
 SHS_results_path_for_AMM = r"SHS_pretend_results_empty.csv"
 
 #SHS_results_path = r"C:\Users\lawashburn\Documents\HyPep1.0\HyPep_GUI_linked_final_v2\input_files\SHS_pretend_results.csv"
